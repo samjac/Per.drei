821754d0a9fadd3223e90927016fde528c17dbe2
samjac <30877998+samjac@users.noreply.github.com>
2017-08-14 10:33:57 -1000

    updated (2) files
    added function suNombre() to script.js 
    added a button to boxes.html

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var x="";
+function suNombre(){
+  // I am creating a function called suNombre//
+  var userName = prompt("What is your name?");
+  //I created a variable called userName that stores the user's info//
+  window.alert("Bonjour " + userName);
+  //Pop up a window that says hello to the user//
+}

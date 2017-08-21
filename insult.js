fe46394585dce2815421695e66f0f3f3f0684047
samjac <30877998+samjac@users.noreply.github.com>
2017-08-21 10:32:24 -1000

    Add files via upload

---
+++ insult.js
@@ -0,0 +1,11 @@
+var insults = ["Baka desu.", "Not today","Hai.", "Iie."];
+
+function insultGame() {
+
+  var urChoice = [Math.floor(Math.random()*3)];
+
+window.alert(insults[urChoice]);
+}
+
+
+    

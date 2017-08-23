b2e77fae2b984085eb84d51c0a52a4dbf60513cf
samjac <30877998+samjac@users.noreply.github.com>
2017-08-23 10:24:14 -1000

    added findArea.js file
    asked for width and height from user and found the area with a function

---
+++ findArea.js
@@ -0,0 +1,17 @@
+var w = prompt("Give me the width.");
+//ask user for a width and save as w
+var h = prompt("Give me the height.");
+//ask user for a height and save as h
+
+function findArea(width,height) {
+//create a function that takes two parameters
+  var area = width*height;
+//Multiply width and height parameters
+  return value;
+//value is returned
+}
+
+var wallOne = findArea(w,h);
+//pass w and h to the findArea function
+window.alert(wallOne);
+//popup area to screen

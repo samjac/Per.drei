cd98f0d774cf0cea20cc8421ea13df9cf038adc8
samjac <30877998+samjac@users.noreply.github.com>
2017-08-30 10:15:39 -1000

    Add files via upload

---
+++ cplxOBJ.js
@@ -0,0 +1,27 @@
+var stud = {
+  name: "Stefano",
+  age: 18,
+  grade: 12,
+  friends: ["Dennis",
+            "Leo",
+            "Inga"]
+};
+var stud2 = {
+  name: "Misaki",
+  age: 17,
+  grade: 12,
+  friends: ["Usui",
+            "Taiga",
+            "Ryuuji"]
+};
+var stud3 = {
+  name: "Marinette",
+  age: 15,
+  grade: 10,
+  friends: ["Adrien",
+            "Aliyah",
+            "Chloe"]
+};
+
+var x = [stud,stud2,stud3];
+window.alert(x[1].friends[1]);
